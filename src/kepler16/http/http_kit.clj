7b8e295e5328f6cdb39b9875c9eaa5b8c60deb30
Alexis Vincent <mail@alexisvincent.io>
2021-04-27 15:12:48 +0100

    Stop using integrant

--- src/kepler16/http/http_kit.clj
+++ src/kepler16/http/http_kit.clj
@@ -1,25 +1,11 @@
 (ns kepler16.http.http-kit
-  (:require [org.httpkit.server :as http-kit]
-            [integrant.core :as ig]))
+  (:require [org.httpkit.server :as http-kit]))
 
-(defmethod ig/init-key :adapter/http-kit [_ opts]
-  (let [handler (atom (delay (:handler opts)))
-        options (-> opts (dissoc :handler) (assoc :join? false))]
-    {:handler handler
-     :server  (http-kit/run-server (fn [req] (@@handler req)) options)}))
+(def defaults {:join? false})
 
-(defmethod ig/halt-key! :adapter/http-kit [_ {:keys [server]}]
-  (.stop server))
-
-(defmethod ig/suspend-key! :adapter/http-kit [_ {:keys [handler]}]
-  (reset! handler (promise)))
-
-(defmethod ig/resolve-key :adapter/http-kit [_ {:keys [server]}]
-  server)
-
-(defmethod ig/resume-key :adapter/http-kit [key opts old-opts old-impl]
-  (if (= (dissoc opts :handler) (dissoc old-opts :handler))
-    (do (deliver @(:handler old-impl) (:handler opts))
-        old-impl)
-    (do (ig/halt-key! key old-impl)
-        (ig/init-key key opts))))
+(comment
+  {:http/server {:start `(org.httpkit.server/run-jetty
+                          handler
+                          (merge kepler16.http.http-kit/defaults
+                                 {}))
+                 :stop `(.close this)}})
--- src/kepler16/http/jetty.clj
+++ src/kepler16/http/jetty.clj
@@ -1,25 +1,13 @@
 (ns kepler16.http.jetty
-  (:require [ring.adapter.jetty :as jetty]
-            [integrant.core :as ig]))
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defmethod ig/init-key :adapter/jetty [_ opts]
-  (let [handler (atom (delay (:handler opts)))
-        options (-> opts (dissoc :handler) (assoc :join? false))]
-    {:handler handler
-     :server  (jetty/run-jetty (fn [req] (@@handler req)) options)}))
+(def defaults {:join? false})
 
-(defmethod ig/halt-key! :adapter/jetty [_ {:keys [server]}]
-  (.stop server))
+(comment
+  (defn handler [req]
+    {:body "hi"})
 
-(defmethod ig/suspend-key! :adapter/jetty [_ {:keys [handler]}]
-  (reset! handler (promise)))
-
-(defmethod ig/resolve-key :adapter/jetty [_ {:keys [server]}]
-  server)
-
-(defmethod ig/resume-key :adapter/jetty [key opts old-opts old-impl]
-  (if (= (dissoc opts :handler) (dissoc old-opts :handler))
-    (do (deliver @(:handler old-impl) (:handler opts))
-        old-impl)
-    (do (ig/halt-key! key old-impl)
-        (ig/init-key key opts))))
+  {:http/server (let [jetty-options (merge kepler16.http.jetty/defaults {:port 8080})
+                      handler #'handler]
+                  {:start `(ring.adapter.jetty/run-jetty ~handler ~jetty-options)
+                   :stop '(.stop this)})})
